d2caf13384fdc304b597ec14fbeaa3c74e036d22
Abhishek Nagekar <abhishek@nagekar.com>
2018-08-18 19:02:26 +0530

    footer converted to functional component

--- src/landing-page/components/footer.js
+++ src/landing-page/components/footer.js
@@ -1,18 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { NavWrapper, NavInner, FooterText, NavCompany } from '../common-styles';
 
-class Footer extends Component {
-    render() {
-        return (
-            <NavWrapper>
-                <NavInner>
-                    <FooterText>
-                        Made with &hearts; for <NavCompany><i>CAREER<b>FOUNDRY</b></i></NavCompany>
-                    </FooterText>
-                </NavInner>
-            </NavWrapper>
-        )
-    }
+/**
+ * 
+ */
+const Footer = () => {
+    return (
+        <NavWrapper>
+            <NavInner>
+                <FooterText>
+                    Made with &hearts; for <NavCompany><i>CAREER<b>FOUNDRY</b></i></NavCompany>
+                </FooterText>
+            </NavInner>
+        </NavWrapper>
+    )
 }
 
 export default Footer;
